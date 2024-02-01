6b17fc38cd5aa6835dd97e7e832a5616ad8660da
soezyxstt <13122080@mahasiswa.itb.ac.id>
2024-02-01 15:59:33 +0700

    remove font variable

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -3,24 +3,19 @@ import localFont from "next/font/local";
 const header = localFont({
   src: "fonts/PropagationRegular.ttf",
   display: "swap",
-  variable: "--font-header",
 });
 const body = localFont({
   src: "/fonts/TangoSans.ttf",
   display: "swap",
-  variable: "--font-body",
 });
 const bodyBold = localFont({
   src: "/fonts/TangoSans_Bold.ttf",
-  variable: "--font-body-bold",
 });
 const bodyBoldItalic = localFont({
   src: "/fonts/TangoSans_BoldItalic.ttf",
-  variable: "--font-body-bold-italic",
 });
 const bodyItalic = localFont({
   src: "/fonts/TangoSans_Italic.ttf",
-  variable: "--font-body-italic",
 });
 
 export { header, body, bodyBold, bodyBoldItalic, bodyItalic };
