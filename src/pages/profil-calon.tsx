06b1b6813323ed93d0417821746c55ae541a4f88
soezyxstt <13122080@mahasiswa.itb.ac.id>
2024-02-02 09:34:08 +0700

    minor change

--- src/pages/profil-calon.tsx
+++ src/pages/profil-calon.tsx
@@ -3,7 +3,7 @@ import { header } from '~/styles/fonts';
 
 const ProfilCalon = () => {
   return (
-    <div className='flex justify-center items-center'>
+    <div className='flex justify-center items-center dusty-bg'>
       <Bg />
       <h1 className={`${header.className} text-shadow shadow-teal-4 text-oren text-stroke-width-1 text-stroke-color-cream text-4xl z-[1]`}>Profil Calon</h1>
     </div>
