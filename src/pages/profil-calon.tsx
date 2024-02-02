5b9cef790ee1649cea7182d2f59916d29c392f18
TazakiN <tazkia.nizami@gmail.com>
2024-02-02 21:22:33 +0700

    feat: responsive page (data card)

--- src/pages/profil-calon.tsx
+++ src/pages/profil-calon.tsx
@@ -1,4 +1,4 @@
-import { header, bodyBold } from "@fonts";
+import { header } from "@fonts";
 import Image from "next/image";
 import Oval2 from "../../public/properti/oval_2.png";
 import Bg from "~/components/background";
@@ -18,112 +18,110 @@ import {
 
 const ProfilCalon = () => {
   return (
-    <div className="justify-content dusty-bg w-full items-center">
+    <div className="justify-content dusty-bg w-full items-center pb-16">
       <Bg />
       <div>
-        <div>
-          <div /* Text K3M */
-            className={`${header.className} pt-20 text-center text-6xl font-normal not-italic text-navy drop-shadow-lg`}
+        <div /* Text K3M */
+          className={`${header.className} pt-20 text-center text-6xl font-normal not-italic text-navy drop-shadow-lg`}
+        >
+          {"K3M"}
+        </div>
+        <div className="flex w-full flex-col p-14 md:flex-row md:space-x-16 ">
+          <div /* Photo Carousel */
+            className="border-box flex-shrink-0 flex-grow-0"
           >
-            {"K3M"}
+            <Carousel className="relative w-full max-w-xs">
+              <CarouselContent>
+                <CarouselItem>
+                  <div>
+                    <Card className="flex h-96 min-w-64 items-center justify-center border-4 border-black bg-blue-5">
+                      <CardContent className="m-0 p-0">
+                        <span className="text-4xl font-semibold">
+                          {"Calon 1"}
+                        </span>
+                      </CardContent>
+                    </Card>
+                    <Image
+                      className="mr-2"
+                      src={Oval2}
+                      height={48}
+                      alt="Oval"
+                    />
+                  </div>
+                </CarouselItem>
+                <CarouselItem>
+                  <div>
+                    <Card className="flex h-96 min-w-64 items-center justify-center border-4 border-black bg-blue-5">
+                      <CardContent className="m-0 p-0">
+                        <span className="text-4xl font-semibold">
+                          {"Calon 2"}
+                        </span>
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+              </CarouselContent>
+              <CarouselPrevious className="absolute -left-6 h-14 w-14" />
+              <CarouselNext className="absolute -right-6 h-14 w-14" />
+            </Carousel>
           </div>
-          <div className="flex w-full space-x-16 p-14">
-            <div /* Photo Carousel */
-              className="border-box flex-shrink-0 flex-grow-0"
-            >
-              <Carousel className="relative w-full max-w-xs">
-                <CarouselContent>
-                  <CarouselItem>
-                    <div>
-                      <Card className="flex h-96 min-w-64 items-center justify-center border-4 border-black bg-blue-5">
-                        <CardContent className="m-0 p-0">
-                          <span className="text-4xl font-semibold">
-                            {"Calon 1"}
-                          </span>
-                        </CardContent>
-                      </Card>
-                      <Image
-                        className="mr-2"
-                        src={Oval2}
-                        height={48}
-                        alt="Oval"
-                      />
-                    </div>
-                  </CarouselItem>
-                  <CarouselItem>
-                    <div>
-                      <Card className="flex h-96 min-w-64 items-center justify-center border-4 border-black bg-blue-5">
-                        <CardContent className="m-0 p-0">
-                          <span className="text-4xl font-semibold">
-                            {"Calon 2"}
-                          </span>
-                        </CardContent>
-                      </Card>
-                    </div>
-                  </CarouselItem>
-                </CarouselContent>
-                <CarouselPrevious className="absolute -left-6 h-14 w-14" />
-                <CarouselNext className="absolute -right-6 h-14 w-14" />
-              </Carousel>
-            </div>
-            <div /* data calon */ className="z-[1] w-full">
-              <DataCalonCard section="K3M" />
-              <div className="flex flex-row items-center pt-1">
-                <Portofolio
-                  linkPorto={"https://id.wikipedia.org/wiki/Portofolio"}
-                />
-                <FaInstagram linkIG={"https://instagram.com"} />
-              </div>
+          <div /* data calon */ className="z-[1] w-full pt-20 md:pt-0">
+            <DataCalonCard section="K3M" />
+            <div className="flex flex-row items-center pt-2">
+              <Portofolio
+                linkPorto={"https://id.wikipedia.org/wiki/Portofolio"}
+              />
+              <FaInstagram linkIG={"https://instagram.com"} />
             </div>
           </div>
         </div>
-        <div>
-          <div /* Text MWA - WM */
-            className={`${header.className} pt-20 text-center text-6xl font-normal not-italic text-red-5 drop-shadow-md`}
+      </div>
+      <div>
+        <div /* Text MWA - WM */
+          className={`${header.className} pt-20 text-center text-6xl font-normal not-italic text-red-5 drop-shadow-md`}
+        >
+          {"MWA - WM"}
+        </div>
+        <div className="flex w-full flex-col p-14 md:flex-row md:space-x-16">
+          <div /* Photo Carousel */
+            className="border-box flex-shrink-0 flex-grow-0"
           >
-            {"MWA - WM"}
+            <Carousel className="relative w-full max-w-xs">
+              <CarouselContent>
+                <CarouselItem>
+                  <div>
+                    <Card className="flex h-96 min-w-64 flex-col items-center justify-center border-4 border-black bg-red-5">
+                      <CardContent className="m-0 p-0">
+                        <span className="text-4xl font-semibold">
+                          {"Calon 1"}
+                        </span>
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+                <CarouselItem>
+                  <div>
+                    <Card className="flex h-96 min-w-64 flex-col items-center justify-center border-4 border-black bg-red-5">
+                      <CardContent className="m-0 p-0">
+                        <span className="text-4xl font-semibold">
+                          {"Calon 2"}
+                        </span>
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+              </CarouselContent>
+              <CarouselPrevious className="absolute -left-6 h-14 w-14" />
+              <CarouselNext className="absolute -right-6 h-14 w-14" />
+            </Carousel>
           </div>
-          <div className="flex w-full space-x-16 p-14">
-            <div /* Photo Carousel */
-              className="border-box flex-shrink-0 flex-grow-0"
-            >
-              <Carousel className="relative w-full max-w-xs">
-                <CarouselContent>
-                  <CarouselItem>
-                    <div>
-                      <Card className="flex h-96 min-w-64 flex-col items-center justify-center border-4 border-black bg-red-5">
-                        <CardContent className="m-0 p-0">
-                          <span className="text-4xl font-semibold">
-                            {"Calon 1"}
-                          </span>
-                        </CardContent>
-                      </Card>
-                    </div>
-                  </CarouselItem>
-                  <CarouselItem>
-                    <div>
-                      <Card className="flex h-96 min-w-64 flex-col items-center justify-center border-4 border-black bg-red-5">
-                        <CardContent className="m-0 p-0">
-                          <span className="text-4xl font-semibold">
-                            {"Calon 2"}
-                          </span>
-                        </CardContent>
-                      </Card>
-                    </div>
-                  </CarouselItem>
-                </CarouselContent>
-                <CarouselPrevious className="absolute -left-6 h-14 w-14" />
-                <CarouselNext className="absolute -right-6 h-14 w-14" />
-              </Carousel>
-            </div>
-            <div /* data calon */ className="z-[1] w-full">
-              <DataCalonCard section="MWA-WM" />
-              <div className="flex flex-row items-center pt-1">
-                <Portofolio
-                  linkPorto={"https://id.wikipedia.org/wiki/Portofolio"}
-                />
-                <FaInstagram linkIG={"https://instagram.com"} />
-              </div>
+          <div /* data calon */ className="z-[1] w-full pt-20 md:pt-0">
+            <DataCalonCard section="MWA-WM" />
+            <div className="flex flex-row items-center pt-2">
+              <Portofolio
+                linkPorto={"https://id.wikipedia.org/wiki/Portofolio"}
+              />
+              <FaInstagram linkIG={"https://instagram.com"} />
             </div>
           </div>
         </div>
