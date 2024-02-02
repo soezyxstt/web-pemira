4d34d404883e4771e64a153938bbb1ab0e5ddba2
TazakiN <tazkia.nizami@gmail.com>
2024-02-02 20:07:34 +0700

    refactor: implement DataCalonCard

--- src/pages/profil-calon.tsx
+++ src/pages/profil-calon.tsx
@@ -4,14 +4,10 @@ import Oval2 from "../../public/properti/oval_2.png";
 import Bg from "~/components/background";
 import FaInstagram from "~/components/ui/profil-calon/faInstagram";
 import Portofolio from "~/components/ui/profil-calon/portofolio";
+import DataCalonCard from "~/components/data-calon-card";
 
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "~/components/ui/card";
+import { Card, CardContent } from "~/components/ui/card";
+
 import {
   Carousel,
   CarouselContent,
@@ -30,7 +26,7 @@ const ProfilCalon = () => {
             className={`${header.className} pt-20 text-center text-6xl font-normal not-italic text-navy drop-shadow-lg`}
           >
             {"K3M"}
-          </div>{" "}
+          </div>
           <div className="flex w-full space-x-16 p-14">
             <div /* Photo Carousel */
               className="border-box flex-shrink-0 flex-grow-0"
@@ -71,51 +67,7 @@ const ProfilCalon = () => {
               </Carousel>
             </div>
             <div /* data calon */ className="z-[1] w-full">
-              <Card className="box-border w-full flex-grow rounded-2xl border-4 border-black bg-blue-5">
-                <CardHeader className="flex flex-row items-baseline drop-shadow-lg">
-                  <CardTitle
-                    className={`${bodyBold.className} text-3xl text-brown-1`}
-                  >
-                    {"Nama Calon"}
-                  </CardTitle>
-                  <CardDescription className="pl-3 align-bottom text-brown-3 drop-shadow-lg">
-                    {"Jurusan Angkatan"}
-                  </CardDescription>
-                </CardHeader>
-                <CardContent>
-                  <CardTitle
-                    className={`${bodyBold.className} flex justify-center text-3xl text-brown-1`}
-                  >
-                    Visi
-                  </CardTitle>
-                  <CardDescription
-                    className={`p-y-2 w-full bg-teal-1 pl-1 text-sm text-black drop-shadow-lg`}
-                  >
-                    {"Visi Calon"}
-                  </CardDescription>
-                  <CardTitle
-                    className={`${bodyBold.className} flex justify-center pt-2 text-3xl text-brown-1`}
-                  >
-                    Misi
-                  </CardTitle>
-                  <div className="flex flex-col space-y-1">
-                    <CardDescription
-                      className={`p-y-2 w-full bg-teal-1 pl-1 text-xs text-black drop-shadow-lg`}
-                    >
-                      rcu dictum. Sit amet consectetur adipiscing elit. Et
-                      malesuada fames ac turpis ultricies tristique nulla
-                      aliquet enim. Diam phasellus vestibulum lorem sed. Dolor
-                      morbi non arcu risus quis varius quam quisque. Enim eu
-                      turpis egestas pretium aenean pharetra magna ac.
-                    </CardDescription>
-                    <CardDescription
-                      className={`p-y-2 w-full bg-teal-1 pl-1 text-xs text-black drop-shadow-lg`}
-                    >
-                      {"Misi Calon"}
-                    </CardDescription>
-                  </div>
-                </CardContent>
-              </Card>
+              <DataCalonCard section="K3M" />
               <div className="flex flex-row items-center pt-1">
                 <Portofolio
                   linkPorto={"https://id.wikipedia.org/wiki/Portofolio"}
@@ -165,51 +117,7 @@ const ProfilCalon = () => {
               </Carousel>
             </div>
             <div /* data calon */ className="z-[1] w-full">
-              <Card className="box-border w-full flex-grow rounded-2xl border-4 border-black bg-red-5">
-                <CardHeader className="flex flex-row items-baseline drop-shadow-lg">
-                  <CardTitle
-                    className={`${bodyBold.className} text-3xl text-brown-1`}
-                  >
-                    {"Nama Calon"}
-                  </CardTitle>
-                  <CardDescription className="pl-3 align-bottom text-brown-3 drop-shadow-lg">
-                    {"Jurusan Angkatan"}
-                  </CardDescription>
-                </CardHeader>
-                <CardContent>
-                  <CardTitle
-                    className={`${bodyBold.className} flex justify-center text-3xl text-brown-1`}
-                  >
-                    Visi
-                  </CardTitle>
-                  <CardDescription
-                    className={`p-y-2 w-full bg-red-3 pl-1 text-sm text-black drop-shadow-lg`}
-                  >
-                    {"Visi Calon"}
-                  </CardDescription>
-                  <CardTitle
-                    className={`${bodyBold.className} flex justify-center pt-2 text-3xl text-brown-1`}
-                  >
-                    Misi
-                  </CardTitle>
-                  <div className="flex flex-col space-y-1">
-                    <CardDescription
-                      className={`p-y-2 w-full bg-red-3 pl-1 text-xs text-black drop-shadow-lg`}
-                    >
-                      rcu dictum. Sit amet consectetur adipiscing elit. Et
-                      malesuada fames ac turpis ultricies tristique nulla
-                      aliquet enim. Diam phasellus vestibulum lorem sed. Dolor
-                      morbi non arcu risus quis varius quam quisque. Enim eu
-                      turpis egestas pretium aenean pharetra magna ac.
-                    </CardDescription>
-                    <CardDescription
-                      className={`p-y-2 w-full bg-red-3 pl-1 text-xs text-black drop-shadow-lg`}
-                    >
-                      {"Misi Calon"}
-                    </CardDescription>
-                  </div>
-                </CardContent>
-              </Card>
+              <DataCalonCard section="MWA-WM" />
               <div className="flex flex-row items-center pt-1">
                 <Portofolio
                   linkPorto={"https://id.wikipedia.org/wiki/Portofolio"}
