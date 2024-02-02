b0216248eb22a28d7bd3508d9b884d718d823a99
TazakiN <tazkia.nizami@gmail.com>
2024-02-02 21:01:37 +0700

    feat: responsive portofolio button

--- src/components/ui/profil-calon/portofolio.tsx
+++ src/components/ui/profil-calon/portofolio.tsx
@@ -5,9 +5,9 @@ import { body } from "@fonts";
 const Portofolio = ({ linkPorto }: { linkPorto: string }) => {
   return (
     <Link href={linkPorto} className="w-full">
-      <div className="box-border items-center justify-center rounded-2xl border-2 border-black bg-brown-5">
+      <div className="box-border items-center justify-center rounded-2xl border-2 border-black bg-brown-5 py-2">
         <div
-          className={`${body.className} flex justify-center text-3xl text-brown-1`}
+          className={`${body.className} flex justify-center text-lg text-brown-1 md:text-3xl`}
         >
           Portofolio
         </div>
