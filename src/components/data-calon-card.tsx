216c443adeee39d7f2293783c0385e2deca42b05
TazakiN <tazkia.nizami@gmail.com>
2024-02-02 21:21:37 +0700

    feat: responsive data-calon-card

--- src/components/data-calon-card.tsx
+++ src/components/data-calon-card.tsx
@@ -16,33 +16,35 @@ const DataCalonCard = ({ section }: { section: string }) => {
     <Card
       className={`box-border w-full flex-grow rounded-2xl border-4 border-black ${bgColor}`}
     >
-      <CardHeader className="flex flex-row items-baseline drop-shadow-lg">
-        <CardTitle className={`${body.className} text-3xl text-brown-1`}>
+      <CardHeader className="flex flex-col items-baseline drop-shadow-lg md:flex-row">
+        <CardTitle
+          className={`${body.className} text-base text-brown-1 md:text-3xl`}
+        >
           {"Nama Calon"}
         </CardTitle>
-        <CardDescription className="pl-3 align-bottom text-brown-3 drop-shadow-lg">
+        <CardDescription className="align-bottom text-sm text-brown-3 drop-shadow-lg md:pl-3 md:text-base">
           {"Jurusan Angkatan"}
         </CardDescription>
       </CardHeader>
       <CardContent>
         <CardTitle
-          className={`${body.className} flex justify-center text-3xl text-brown-1`}
+          className={`${body.className} flex justify-center text-sm text-brown-1 md:text-3xl`}
         >
           Visi
         </CardTitle>
         <CardDescription
-          className={`p-y-2 w-full ${bgDesc} pl-1 text-sm text-black drop-shadow-lg`}
+          className={`p-y-2 w-full ${bgDesc} pl-1 text-[8px] text-black drop-shadow-lg md:text-sm`}
         >
           {"Visi Calon"}
         </CardDescription>
         <CardTitle
-          className={`${body.className} flex justify-center pt-2 text-3xl text-brown-1`}
+          className={`${body.className} flex justify-center pt-2 text-sm text-brown-1 md:text-3xl`}
         >
           Misi
         </CardTitle>
         <div className="flex flex-col space-y-1">
           <CardDescription
-            className={`p-y-2 w-full ${bgDesc} pl-1 text-xs text-black drop-shadow-lg`}
+            className={`p-y-2 w-full ${bgDesc} pl-1 text-[7px] text-black drop-shadow-lg md:text-xs`}
           >
             rcu dictum. Sit amet consectetur adipiscing elit. Et malesuada fames
             ac turpis ultricies tristique nulla aliquet enim. Diam phasellus
@@ -50,7 +52,7 @@ const DataCalonCard = ({ section }: { section: string }) => {
             quisque. Enim eu turpis egestas pretium aenean pharetra magna ac.
           </CardDescription>
           <CardDescription
-            className={`p-y-2 w-full ${bgDesc} pl-1 text-xs text-black drop-shadow-lg`}
+            className={`p-y-2 w-full ${bgDesc} pl-1 text-[7px] text-black drop-shadow-lg md:text-xs`}
           >
             {"Misi Calon"}
           </CardDescription>
