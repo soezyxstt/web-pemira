b1f33bc22d5a6c7aa24eff00b3cf72d6fc913aa6
TazakiN <tazkia.nizami@gmail.com>
2024-02-03 11:33:45 +0700

    feat: use props to fill data calon

--- src/components/data-calon-card.tsx
+++ src/components/data-calon-card.tsx
@@ -1,133 +1,91 @@
 import React from "react";
-import { body } from "@fonts";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "~/components/ui/card";
-import PointDesc from "./ui/profil-calon/pointDesc";
 import {
   Carousel,
   CarouselContent,
-  CarouselItem,
   CarouselNext,
   CarouselPrevious,
 } from "./ui/carousel";
+import DataCalonCarouselItem from "./ui/profil-calon/dataCalonCarouselItem";
 
 const DataCalonCard = ({ section }: { section: string }) => {
-  const bgColor = section === "K3M" ? "bg-blue-5" : "bg-red-5";
-  const bgDesc = section === "K3M" ? "bg-teal-1" : "bg-red-3";
-  const nextButton = section === "K3M" ? "data-next-K3M" : "data-next-MWA-WM";
-  const prevButton = section === "K3M" ? "data-prev-K3M" : "data-prev-MWA-WM";
+  const nextButtonID = section === "K3M" ? "data-next-K3M" : "data-next-MWA-WM";
+  const prevButtonID = section === "K3M" ? "data-prev-K3M" : "data-prev-MWA-WM";
 
-  return (
-    <Carousel>
-      <CarouselContent>
-        <CarouselItem>
-          <Card
-            className={`box-border w-full flex-grow rounded-2xl border-4 border-black ${bgColor}`}
-          >
-            <CardHeader className="flex flex-col items-baseline drop-shadow-lg md:flex-row">
-              <CardTitle
-                className={`${body.className} text-base text-brown-1 md:text-3xl`}
-              >
-                {"Nama Calon 1"}
-              </CardTitle>
-              <CardDescription className="align-bottom text-sm text-brown-3 drop-shadow-lg md:pl-3 md:text-base">
-                {"Jurusan Angkatan"}
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <CardTitle
-                className={`${body.className} flex justify-center text-sm text-brown-1 md:text-3xl`}
-              >
-                Visi
-              </CardTitle>
-              <PointDesc tipe={"visi"} bgDesc={bgDesc} point={"Visi Calon"} />
-              <CardTitle
-                className={`${body.className} flex justify-center pt-2 text-sm text-brown-1 md:text-3xl`}
-              >
-                Misi
-              </CardTitle>
-              <div className="flex flex-col space-y-1">
-                <PointDesc
-                  tipe={"misi"}
-                  bgDesc={bgDesc}
-                  point={
-                    "rcu dictum. Sit amet consectetur adipiscing elit. Et malesuada famesac turpis ultricies tristique nulla aliquet enim. Diam phasellusvestibulum lorem sed. Dolor morbi non arcu risus quis varius quamquisque. Enim eu turpis egestas pretium aenean pharetra magna ac."
-                  }
-                />
-                <PointDesc bgDesc={bgDesc} point={"Misi Calon"} tipe={"misi"} />
-                <PointDesc
-                  bgDesc={bgDesc}
-                  point={
-                    "Dolor morbi non arcu risus quis varius quamquisque. Enim eu turpis egestas pretium aenean pharetra magna ac."
-                  }
-                  tipe={"misi"}
-                />
-              </div>
-            </CardContent>
-          </Card>
-        </CarouselItem>
-        <CarouselItem>
-          <Card
-            className={`box-border w-full flex-grow rounded-2xl border-4 border-black ${bgColor}`}
-          >
-            <CardHeader className="flex flex-col items-baseline drop-shadow-lg md:flex-row">
-              <CardTitle
-                className={`${body.className} text-base text-brown-1 md:text-3xl`}
-              >
-                {"Nama Calon 2"}
-              </CardTitle>
-              <CardDescription className="align-bottom text-sm text-brown-3 drop-shadow-lg md:pl-3 md:text-base">
-                {"Jurusan Angkatan"}
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <CardTitle
-                className={`${body.className} flex justify-center text-sm text-brown-1 md:text-3xl`}
-              >
-                Visi
-              </CardTitle>
-              <PointDesc tipe={"visi"} bgDesc={bgDesc} point={"Visi Calon"} />
-              <CardTitle
-                className={`${body.className} flex justify-center pt-2 text-sm text-brown-1 md:text-3xl`}
-              >
-                Misi
-              </CardTitle>
-              <div className="flex flex-col space-y-1">
-                <PointDesc
-                  tipe={"misi"}
-                  bgDesc={bgDesc}
-                  point={
-                    "rcu dictum. Sit amet consectetur adipiscing elit. Et malesuada famesac turpis ultricies tristique nulla aliquet enim. Diam phasellusvestibulum lorem sed. Dolor morbi non arcu risus quis varius quamquisque. Enim eu turpis egestas pretium aenean pharetra magna ac."
-                  }
-                />
-                <PointDesc bgDesc={bgDesc} point={"Misi Calon"} tipe={"misi"} />
-                <PointDesc
-                  bgDesc={bgDesc}
-                  point={
-                    "Dolor morbi non arcu risus quis varius quamquisque. Enim eu turpis egestas pretium aenean pharetra magna ac."
-                  }
-                  tipe={"misi"}
-                />
-              </div>
-            </CardContent>
-          </Card>
-        </CarouselItem>
-      </CarouselContent>
-      <CarouselPrevious
-        className="absolute -left-6 hidden h-14 w-14"
-        id={prevButton}
-      />
-      <CarouselNext
-        className="absolute -right-6 hidden h-14 w-14"
-        id={nextButton}
-      />
-    </Carousel>
-  );
+  if (section === "K3M") {
+    return (
+      <Carousel>
+        <CarouselContent>
+          <DataCalonCarouselItem
+            section={"K3M"}
+            namaCalon={"Yadi Supriyadi"}
+            jurusanAngkatan={"TA'21"}
+            visi={"cerah"}
+            misi={["hidup", "selamet", "adibing selamet ketemu jaka sembung"]}
+            linkPorto={""}
+            linkIG={""}
+          />
+          <DataCalonCarouselItem
+            section={"K3M"}
+            namaCalon={"Erik Tohir"}
+            jurusanAngkatan={"Mesin'13"}
+            visi={"PSSI nomer 1"}
+            misi={[
+              "memberantas isis",
+              "menolak al-qaeda",
+              "menghancurkan taliban",
+            ]}
+            linkPorto={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"}
+            linkIG={"https://www.instagram.com/eriktohir/"}
+          />
+        </CarouselContent>
+        <CarouselPrevious
+          className="absolute -left-6 hidden h-14 w-14"
+          id={prevButtonID}
+        />
+        <CarouselNext
+          className="absolute -right-6 hidden h-14 w-14"
+          id={nextButtonID}
+        />
+      </Carousel>
+    );
+  } else {
+    return (
+      <Carousel>
+        <CarouselContent>
+          <DataCalonCarouselItem
+            section={"MWA-WM"}
+            namaCalon={"Tatang Nurjaman"}
+            jurusanAngkatan={"TA'21"}
+            visi={"cerah"}
+            misi={["hidup", "selamet", "adibing selamet ketemu jaka sembung"]}
+            linkPorto={""}
+            linkIG={""}
+          />
+          <DataCalonCarouselItem
+            section={"MWA-WM"}
+            namaCalon={"Erik Tohir"}
+            jurusanAngkatan={"Mesin'13"}
+            visi={"PSSI nomer 1"}
+            misi={[
+              "memberantas isis",
+              "menolak al-qaeda",
+              "menghancurkan taliban",
+            ]}
+            linkPorto={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"}
+            linkIG={"https://www.instagram.com/eriktohir/"}
+          />
+        </CarouselContent>
+        <CarouselPrevious
+          className="absolute -left-6 hidden h-14 w-14"
+          id={prevButtonID}
+        />
+        <CarouselNext
+          className="absolute -right-6 hidden h-14 w-14"
+          id={nextButtonID}
+        />
+      </Carousel>
+    );
+  }
 };
 
 export default DataCalonCard;
