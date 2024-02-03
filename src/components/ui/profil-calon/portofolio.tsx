4f43aa7958e4931c31cc952b84d51fa166877b3d
TazakiN <tazkia.nizami@gmail.com>
2024-02-03 12:50:38 +0700

    fix: change portofolio border width

--- src/components/ui/profil-calon/portofolio.tsx
+++ src/components/ui/profil-calon/portofolio.tsx
@@ -5,7 +5,7 @@ import { body } from "@fonts";
 const Portofolio = ({ linkPorto }: { linkPorto: string }) => {
   return (
     <Link href={linkPorto} className="w-full">
-      <div className="box-border items-center justify-center rounded-2xl border-2 border-black bg-brown-5 py-2">
+      <div className="box-border items-center justify-center rounded-2xl border-[3px] border-black bg-brown-5 py-2">
         <div
           className={`${body.className} flex justify-center text-lg text-brown-1 transition-transform duration-300 ease-in-out hover:scale-110 md:text-3xl`}
         >
