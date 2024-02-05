97319084ba1cf0616b7b36ce86a2cba93c77b0bd
Rafly Hanggaraksa <ajarafly422@gmail.com>
2024-02-05 21:35:06 +0700

    fix: responsive for api

--- src/components/lokasi-voting.tsx
+++ src/components/lokasi-voting.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'; 
 import { ChevronRight,ChevronLeft } from "lucide-react"
 import { Button } from "~/components/ui/button"
-import { header } from '~/styles/fonts';
+import { header,body } from '~/styles/fonts';
 
 const LokasiVoting = () => {
     const [currentIdx,setCurrentIdx] = useState(0);
@@ -32,26 +32,25 @@ const LokasiVoting = () => {
         >
             Lokasi
         </h1>
-        <div className='bg-red-1 lg:pt-5 lg:px-5 w-[60vw] rounded-lg overflow-clip'>
+        <div className='bg-red-1 lg:pt-5 lg:px-5 w-[60vw] rounded-[2rem] overflow-clip'>
           <div>
             <iframe
               src={srcLokasi[currentIdx]}
-              width="100%"
               height="400px"
               loading="lazy"
-              className='rounded-lg'
+              className='rounded-[1rem] w-[100%] max-[500px]:w-[101%]'
             ></iframe>
           </div>
           <div className='flex flex-row justify-center'>
-            <div className='mx-5 my-4 px-3 py-2 bg-red-3 rounded-lg w-fit flex flex-row justify-center items-center gap-6'>
+            <div className='mx-5 my-4 px-3 py-2 bg-red-3 rounded-lg w-fit flex flex-row justify-center items-center gap-6 min-[320px]:gap-3'>
               {/* for carousel */}
-                <Button onClick={cycleBackward} size="icon" variant="secondary" className='rounded-full'>
+                <Button onClick={cycleBackward} size="icon" variant="secondary" className='rounded-full py-1 max-[450px]:w-[25px] max-[450px]:h-[25px]'>
                   <ChevronLeft className="h-4 w-4 text-red-3" strokeWidth={3}/>
                 </Button>
-                <div className='font-body text-white antialiased text-center lg:w-[100px]'>
+                <div className={`${body.className} text-white antialiased text-center lg:w-[100px] lg:text-base md:text-[13px] min-[320px]:text-[11px]`}>
                   {keyLokasi[currentIdx]}
                 </div>
-                <Button onClick={cycleForward} size="icon" variant="secondary" className='rounded-full'>
+                <Button onClick={cycleForward} size="icon" variant="secondary" className='rounded-full max-[450px]:w-[25px] max-[450px]:h-[25px]'>
                   <ChevronRight className="h-4 w-4 text-red-3" strokeWidth={3}/>
                 </Button>
             </div>
