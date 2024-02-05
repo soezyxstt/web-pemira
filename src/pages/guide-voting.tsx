ef4f3905407b705ed649729c23a31bdf355bcfe4
varell <ryukaa57@gmail.com>
2024-02-05 22:02:37 +0700

    penambahan guide votting

--- src/pages/guide-voting.tsx
+++ src/pages/guide-voting.tsx
@@ -1,13 +1,15 @@
-import Bg from '~/components/background';
-import { header } from '~/styles/fonts';
+import Bg from "~/components/background";
+import { header } from "~/styles/fonts";
+import React from "react";
+import Voting from "~/components/voting";
 
 const GuideVoting = () => {
   return (
-    <div className='flex items-center justify-center flex-col dusty-bg'>
+    <div className="dusty-bg flex flex-col items-center justify-center">
       <Bg />
-      <h1 className={`${header.className} text-shadow shadow-teal-4 text-oren text-stroke-width-1 text-stroke-color-cream text-4xl z-[1]`}>Guide Voting</h1>
+      <Voting />
     </div>
   );
-}
+};
 
 export default GuideVoting;
