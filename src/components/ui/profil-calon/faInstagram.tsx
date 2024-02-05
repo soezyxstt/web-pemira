f9c9eb5b9a2fb1bde8aa1b6d44bbd2d0345d38ae
TazakiN <tazkia.nizami@gmail.com>
2024-02-05 21:43:50 +0700

    fix: instagram logo dimension

--- src/components/ui/profil-calon/faInstagram.tsx
+++ src/components/ui/profil-calon/faInstagram.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 
 const FaInstagram = ({ linkIG }: { linkIG: string }) => {
   return (
-    <div
-      /*logo ig */ className="relative ml-2 flex items-center justify-center"
+    <div /*logo ig */
+      className="relative ml-2 flex items-center justify-center"
     >
       <Link href={linkIG}>
         <Image
@@ -13,7 +13,7 @@ const FaInstagram = ({ linkIG }: { linkIG: string }) => {
           height={60}
           width={60}
           alt="Instagram"
-          className="overflow-auto pb-2 pr-2 pt-1 transition-transform duration-300 ease-in-out hover:scale-110 md:h-14 md:w-14"
+          className="p-1 transition-transform duration-300 ease-in-out hover:scale-110 md:h-16 md:w-16"
         ></Image>
       </Link>
     </div>
