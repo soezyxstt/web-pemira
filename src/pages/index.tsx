6511ee2b1e64f54c62cbb469b4e0e2e75735d730
Andhikafdh <andhikafadhillah2211@gmail.com>
2024-02-06 22:17:54 +0700

    countdown pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,41 @@
 import { header } from "@fonts";
+import { body } from "@fonts";
 import Bg from "~/components/background";
+
+const CountdownCard = () => {
+  return (
+    <>
+      <div >
+        <div className="h-52 w-36 bg-[#F4D376]"></div>
+        <div className="h-52 w-36 bg-[#EDA537]"></div>
+      </div>
+    </>
+  );
+};
 
 export default function Home() {
   return (
     <>
       <div className="dusty-bg flex flex-col items-center justify-center">
         <Bg />
-        <div
-          className={`${header.className} z-[1] flex flex-col gap-3 text-center text-4xl text-oren shadow-teal-4 text-shadow text-stroke-width-1 text-stroke-color-cream md:flex-row md:gap-2`}
-        >
-          <h1>PEMIRA</h1>
-          <h1 className="hidden md:block">-</h1>
-          <h1>Coming Soon</h1>
+        <div className="z-10">
+          <h1
+            className={`${header.className} text-center text-5xl text-teal-5`}
+          >
+            PEMILU RAYA KM ITB 2024
+          </h1>
+          <h1
+            className={`${body.className} text-center text-xl text-[#01577C]`}
+          >
+            Lorem Ipsum dolor sir amet
+          </h1>
+          <h1
+            className={`${header.className} text-center text-xs text-[#840202]`}
+          >
+            #GERAKINKLUSIF
+          </h1>
+
+          <CountdownCard />
         </div>
       </div>
     </>
