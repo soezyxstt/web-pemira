a35069deb04f4fd21fff97701b4a9262b76f5e38
Rafly Hanggaraksa <ajarafly422@gmail.com>
2024-02-06 09:04:34 +0700

    feat(faq): added faq

---
+++ src/components/faq.tsx
@@ -0,0 +1,33 @@
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+  } from "~/components/ui/accordion"
+import { header,body } from '~/styles/fonts'
+import Pertanyaan from '~/questions.json'
+
+
+const FaQ = () => {
+    return (
+        <div className="z-[1] mb-[3em]">
+            <h1 className={`${header.className} text-oren text-4xl mb-10 text-center`}>
+                FaQ
+            </h1>
+            <Accordion type="single" collapsible className="w-[60vw] flex flex-col gap-8">
+                {Pertanyaan.map((name,key)=>(
+                    <AccordionItem value={`${key}`} key={key} className="bg-yellow-500 rounded-[2rem]">
+                        <AccordionTrigger className={`bg-oren text-white text-lg tracking-wider px-6 pb-3 rounded-[2rem]`}>
+                            {name.question}
+                        </AccordionTrigger>
+                        <AccordionContent className={`${body.className} text-white mx-5 mt-3 tracking-wide text-lg`}>
+                            {name.answer}
+                        </AccordionContent>
+                    </AccordionItem>
+                ))}
+            </Accordion>
+        </div>
+    )
+}
+
+export default FaQ
--- src/pages/guide-voting.tsx
+++ src/pages/guide-voting.tsx
@@ -1,19 +1,23 @@
 import Bg from '~/components/background';
 import { header } from '~/styles/fonts';
 import LokasiVoting from '~/components/lokasi-voting';
+import FaQ from '~/components/faq';
+
+
 
 const GuideVoting = () => {
   return (
-    <div className='flex items-center justify-center flex-col dusty-bg'>
+    <div className='flex items-center justify-center flex-col dusty-bg gap-[5em]'>
       <Bg />
       <div className={`${header.className} flex-col md:flex-row flex gap-3 md:gap-2 text-shadow shadow-teal-4 text-oren text-stroke-width-1 text-stroke-color-cream text-4xl z-[1] text-center`}>
         <h1>Guide Voting</h1>
         <h1 className='hidden md:block'>-</h1>
         <h1>Coming Soon</h1>
       </div>
-      {/* <h1 className={`${header.className} text-shadow shadow-teal-4 text-oren text-stroke-width-1 text-stroke-color-cream text-4xl z-[1]`}>Guide Voting</h1> */}
-    <LokasiVoting/>
-    </div>
+      <LokasiVoting/>
+      <FaQ/>
+        {/* <h1 className={`${header.className} text-shadow shadow-teal-4 text-oren text-stroke-width-1 text-stroke-color-cream text-4xl z-[1]`}>Guide Voting</h1> */}
+   </div>
   );
 }
 
