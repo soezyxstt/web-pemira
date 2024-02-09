40b2311ed5bb97b0c9a9e9234f55f2d88ccf4c47
Muhammad Reivaldi <reivaldimuhammad25032004@gmail.com>
2024-02-09 15:57:14 +0700

    cardInfo di votingPage

--- src/components/voting/cardInfo.tsx
+++ src/components/voting/cardInfo.tsx
@@ -1,25 +1,50 @@
+import Image from "next/image";
+import { body } from "~/styles/fonts";
+
 interface CardInfoProps {
   name: string;
   jurusan: string;
+  angkatan: string;
   visi: string;
   moto: string;
   photoSrc: string;
 }
 
-const CardInfo = ({ name, jurusan, visi, moto, photoSrc }: CardInfoProps) => {
+const CardInfo = ({
+  name,
+  jurusan,
+  angkatan,
+  visi,
+  moto,
+  photoSrc,
+}: CardInfoProps) => {
   return (
-    <div className="flex flex-col items-center justify-center gap-4 rounded-xl bg-white p-4 shadow-lg border-red-4">
-      <h1 className="text-2xl font-bold text-brown-3">{name}</h1>
-      <p className="text-center text-brown-3">{jurusan}</p>
-      <h1 className="text-4xl font-bold text-red-5">{visi}</h1>
-      <button className="rounded-md bg-red-3 px-4 py-2 text-lg font-bold text-white">
-        {moto}
-      </button>
-      {/* <img
-        src={photoSrc}
-        alt="photo"
-        className="w-32 h-32 rounded-full object-cover"
-      /> */}
+    <div className=" relative flex h-32 w-full rounded-xl border-[3px] border-red-5 shadow shadow-black ">
+      <div className="flex w-32">
+        <Image
+          src={photoSrc}
+          alt="calon"
+          width={70}
+          height={70}
+          className="flex-1"
+        />
+      </div>
+      <div
+        className={`${body.className}  flex flex-1 flex-col rounded-br-xl rounded-tr-xl bg-brown-1 text-center text-red-5 `}
+      >
+        <Image
+          src="/bintang/16.png"
+          alt="bintang"
+          width={60}
+          height={60}
+          className="absolute -left-6 -top-6 rotate-45"
+        />
+        <h1 className="text-xl">{name}</h1>
+        <p className="text-xs">{`${jurusan}'${angkatan}`}</p>
+        <p className="text-xs">{visi}</p>
+        <div className="flex-1"></div>
+        <p className="m-1 text-xs">{moto}</p>
+      </div>
     </div>
   );
 };
--- src/components/voting/firstPage.tsx
+++ src/components/voting/firstPage.tsx
@@ -1,16 +1,24 @@
-import { HTMLAttributes } from "react";
+import { type HTMLAttributes } from "react";
 import Card from "./cardSelect";
 import { cn } from "~/lib/utils";
-import { useSearchParams } from 'next/navigation';
-import { names } from '~/data/k3m';
+import { useSearchParams } from "next/navigation";
+import { names } from "~/data/k3m";
+import CardInfo from "./cardInfo";
 
-const FirstPage = ({ className }: {} & HTMLAttributes<HTMLDivElement>) => {
+const FirstPage = ({ className }: HTMLAttributes<HTMLDivElement>) => {
   const searchParams = useSearchParams();
   const k3m = searchParams.get("k3m");
   return (
     <div className={cn("flex w-[calc(100vw-6rem)] gap-8", className)}>
-      <div className="flex w-[35%] flex-col rounded-lg border-[4px] border-black">
-        <h1></h1>
+      <div className="flex w-[35%] flex-col rounded-lg border-[4px] border-black p-8">
+        <CardInfo
+          name="Nama"
+          visi="Visi"
+          jurusan="Jurusan"
+          angkatan="Angkatan"
+          photoSrc="/logo.png"
+          moto="Tagline / motto"
+        />
       </div>
       <div className="relative flex flex-1 flex-col items-center gap-6 rounded-lg border-[4px] border-blue-5 bg-brown-1 px-10 py-6">
         <button
@@ -25,7 +33,7 @@ const FirstPage = ({ className }: {} & HTMLAttributes<HTMLDivElement>) => {
           {names.map((name, index) => (
             <Card
               nama={name}
-              nomor={(index + 1).toString().padStart(2, '0')}
+              nomor={(index + 1).toString().padStart(2, "0")}
               foto="/logo.png"
               variant="blue"
               key={index}
@@ -33,7 +41,13 @@ const FirstPage = ({ className }: {} & HTMLAttributes<HTMLDivElement>) => {
             />
           ))}
         </div>
-        <Card isKotakKosong variant="blue" className="h-[16vh]" nomor="04" chosenK3M={k3m ?? ""} />
+        <Card
+          isKotakKosong
+          variant="blue"
+          className="h-[16vh]"
+          nomor="04"
+          chosenK3M={k3m ?? ""}
+        />
       </div>
     </div>
   );
