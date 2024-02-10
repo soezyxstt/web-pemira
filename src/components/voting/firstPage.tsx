47332506947aaf6dd1614d6553ee0b0639e535a2
soezyxstt <13122080@mahasiswa.itb.ac.id>
2024-02-10 17:53:22 +0700

    minor change

--- src/components/voting/firstPage.tsx
+++ src/components/voting/firstPage.tsx
@@ -11,7 +11,7 @@ const FirstPage = ({ className }: HTMLAttributes<HTMLDivElement>) => {
   const mwa_wm = searchParams.get("mwa_wm");
   return (
     <div className={cn("flex w-[calc(100vw-6rem)] gap-8", className)}>
-      <div className="relative flex w-[35%] flex-col items-center justify-evenly gap- rounded-lg border-[4px] border-black px-8 py-4">
+      <div className="relative flex w-[35%] flex-col items-center justify-evenly rounded-lg border-[4px] border-black px-8 py-4">
         <img
           src="/texture/frame_copy.png"
           alt="bintang"
