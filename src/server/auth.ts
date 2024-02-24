7f8713bf762c00739f44be9dd4a062c219107ed5
satriadhikara <satriadhikara@gmail.com>
2024-02-24 13:16:22 +0700

    refactor auth

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,4 +1,4 @@
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
+// import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { type GetServerSidePropsContext } from "next";
 import {
   getServerSession,
@@ -6,12 +6,12 @@ import {
   type NextAuthOptions,
   type DefaultUser,
 } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import { prisma } from "~/server/db";
-import { compare } from "bcrypt-ts"
+// import CredentialsProvider from "next-auth/providers/credentials";
+// import { prisma } from "~/server/db";
+// import { compare } from "bcrypt-ts";
 
-import { env } from "~/env";
-import { TRPCError } from '@trpc/server';
+// import { env } from "~/env";
+// import { TRPCError } from "@trpc/server";
 
 declare module "next-auth" {
   interface Session extends DefaultSession {
@@ -29,92 +29,7 @@ declare module "next-auth" {
 }
 
 export const authOptions: NextAuthOptions = {
-  session: {
-    strategy: "jwt",
-  },
-  jwt: {
-    // The maximum age of the NextAuth.js issued JWT in seconds.
-    // Defaults to `session.maxAge`.
-    maxAge: env.SESSION_MAXAGE,
-  },
-  callbacks: {
-    session: ({ session, token }) => ({
-      ...session,
-      user: {
-        ...session.user,
-        id: token.id,
-        nim: token.nim,
-      },
-    }),
-    jwt: ({ token, user }) => {
-      if (user) {
-        token.id = user.id;
-        token.nim = user.nim;
-      }
-      return token;
-    },
-  },
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    CredentialsProvider({
-      id: "credentials",
-      name: "Credentials",
-      credentials: {
-        nim: {
-          label: "NIM",
-          type: "text",
-          placeholder: "13122080",
-        },
-        password: {
-          label: "Password",
-          type: "password",
-          placeholder: "password",
-        },
-      },
-      async authorize(credentials) {
-        if (!credentials) {
-          throw new TRPCError({
-            code: "BAD_REQUEST",
-            message: "Credentials not provided",
-          });
-        }
-
-        const { nim, password } = credentials;
-        if (!nim || !password) {
-          throw new TRPCError({
-            code: "BAD_REQUEST",
-            message: "NIM or password not provided",
-          });
-        }
-        const user = await prisma.user.findUnique({
-          where: {
-            NIM: nim,
-          },
-        });
-        if (!user) {
-          throw new TRPCError({
-            code: "BAD_REQUEST",
-            message: "Akun tidak ditemukan",
-          });
-        }
-        console.log("user:", user);
-
-        const isValid = await compare(password, user.passwordHash);
-        if (!isValid) {
-          throw new TRPCError({
-            code: "BAD_REQUEST",
-            message: "Password salah",
-          });
-        }
-
-        return {
-          id: user.id,
-          nim: user.NIM,
-          name: user.name,
-        };
-      },
-    }),
-  ],
+  providers: [],
 };
 
 export const getServerAuthSession = (ctx: {
